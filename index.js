f7ce7a49ec92d2f49d67f93dd1c490cfd806da1c
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-21 23:50:08 +0500

    the 3 files in landing page code

---
+++ index.js
@@ -0,0 +1,15 @@
+function zia() {
+  let a = prompt("print your value");
+  let b = prompt("value is even and odd");
+  if (a % 2 == 0 && b == " even") {
+    for (i = 0; i < b; i++);
+    console.log(`${b}  this is a value even `);
+  } else if (a % 1 == 0 && b == "odd") {
+    for (i = 0; i < b; i++);
+    {
+      console.log(`${b}  this is a value even `);
+    }
+  } else {
+    console.log("none valided this  value");
+  }
+}
---
+++ scriupt.js
@@ -0,0 +1,5 @@
+let a = parseInt(prompt("what is cheken price"));
+let b = parseInt(prompt("your whaght is kg"));
+let c = a / 1000;
+let e = b * c;
+console.log(e);
